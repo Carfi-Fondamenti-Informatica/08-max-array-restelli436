3e6ffc10897088b97e5ad554a2c209e6d6f7b8d2
restelli436 <117980583+restelli436@users.noreply.github.com>
2022-12-12 22:12:16 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
+#include "lib.h"
+
+float trovaMax(float mat[],int n){
+    float a = mat[0];
+
+    for(int i=1;i<n;i++){
+        if(mat[i] > a){
+            a = mat[i];
+        }
+    }
+    return a;
+}
 
