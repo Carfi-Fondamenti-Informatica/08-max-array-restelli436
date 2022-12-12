f353f5c877d140e93c0d1133feb057a91b88729e
restelli436 <117980583+restelli436@users.noreply.github.com>
2022-12-12 22:13:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+int main() {
+  
+  
+    int n = 0;
+    cin >> n;
+    float a[n];
+  
+  
+    for(int i=0; i<n;i++){
+        cin >> a[i];
+    }
+    cout << trovaMax(a,n);
+    return 0;
 }
